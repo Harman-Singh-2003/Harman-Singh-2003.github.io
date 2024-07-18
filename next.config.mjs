3a4b6de557054004fe5397538dbf513fa0d88746
HarmanSingh <harmansingh2003ca@gmail>
2024-07-17 21:35:31 -0400

    Add basepath for pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    basePath: '/repository-name',
+    assetPrefix: '/repository-name/',
+}
+
 export default nextConfig;
