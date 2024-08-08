41aa33738de4ab71a5bb77e023ab78054aa932ae
HarmanSingh <harmansingh2003ca@gmail>
2024-08-07 23:25:18 -0400

    Cleaned up projects section

--- src/app/Components/ProjectCard.tsx
+++ src/app/Components/ProjectCard.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 interface ProjectProps {
   title: string;
-  description: string;
+  description: string | JSX.Element;
   image: string;
   delay?: string;
 }
@@ -43,21 +43,35 @@ const ProjectCard: React.FC<ProjectProps> = ({
   }, []);
 
   return (
-<div
-  ref={ref}
-  className={`p-4 flex rounded-xl ${
-    isVisible ? `animate-fade-in animate-slide-in-bottom` : "opacity-0"
-  }`}
-  style={{ animationDelay: isVisible ? delay : undefined }}
->      <div className="flex flex-col flex-grow">
+    <div
+      ref={ref}
+      className={`p-4 flex rounded-xl ${
+        isVisible ? `animate-fade-in animate-slide-in-bottom` : "opacity-0"
+      }`}
+      style={{ animationDelay: isVisible ? delay : undefined }}
+    >
+      {" "}
+      <div className="flex flex-col flex-grow">
         <div className="flex justify-between items-center">
-          <div className="flex flex-col">
+          <div className="flex flex-row mb-4 md:mb-2 items-center">
             <div className="text-xl">{title}</div>
+            <div className="md:hidden size-8 md:size-24 m-2 relative flex justify-center items-center flex-shrink-0 rounded-md p-1 bg-white bg-opacity-20 backdrop-blur-xl shadow-[inset_0px_0px_40px_0px_rgba(255,255,255,0.25)]">
+              <Image
+                src={image}
+                layout="responsive"
+                width={50}
+                height={50}
+                className="rounded-sm"
+                alt={""}
+              />
+            </div>
           </div>
         </div>
-        <div className="mt-2">{description}</div>
+        <div className="text-sm md:text-base text-slate-300 md:ml-0">
+          {description}
+        </div>
       </div>
-      <div className="size-12 md:size-32 relative flex justify-center items-center">
+      <div className="hidden size-12 md:size-24 m-4 relative md:flex justify-center items-center flex-shrink-0 rounded-2xl p-6 bg-white bg-opacity-10 backdrop-blur-xl shadow-[inset_0px_0px_50px_0px_rgba(255,255,255,0.15)]">
         <Image
           src={image}
           layout="responsive"
--- src/app/data.tsx
+++ src/app/data.tsx
@@ -27,14 +27,18 @@ export const experienceData = [
     date: "May 2024 - Present",
     description: (
       <p>
-        • Developed an entire backend to increase the security team&apos;s efficiency
-        at managing vulnerabilities using <b>Python</b> and <b>FastAPI</b>
-        <br />
-        • Secured access to the backend by implementing <b>Authentication and Authorization</b> using <b>OAuth2</b> and <b>Okta</b>
-        <br />
-        • Created automated deploys to Github pages using custom <b>workflows</b> through Github Actions
-        <br />
-
+         I contributed to enhancing device security by creating a backend API
+        using FastAPI and Python. This API empowered the security team to
+        efficiently manage vulnerable assets. I was involved in the entire
+        development lifecycle, encompassing backend, frontend, and database
+        migrations to deliver a comprehensive solution within my internship. To
+        protect sensitive backend data, I implemented robust authentication and
+        authorization mechanisms utilizing JWTs and Okta. Furthermore, I
+        streamlined the deployment process by automating builds and deploys,
+        enabling the security team to access real-time information on company
+        devices and vulnerabilities. Currently, I am developing a Next.js
+        frontend and assisting in establishing the infrastructure to complete
+        the project.
       </p>
     ),
     image: `${basePath}/Images/CienaLogo.png`,
@@ -43,16 +47,33 @@ export const experienceData = [
     company: "Wave",
     position: "Software Engineer Intern",
     date: "January 2024 - April 2024",
-    description: (<p>
-     Built reusable UI components in React Native and TypeScript, ensuring consistency across the mobile and web applications. I also streamlined data retrieval and caching using GraphQL and Apollo, improving app performance. Also created bug fixes and manage database migrations in Django. Furthermore, I improved monitoring and alerting systems with Terraform and Datadog. My work on AWS S3 buckets led to a significant storage cost reduction by deleting unused data (nearly 9 TB!).
-      </p>),
+    description: (
+      <p>
+        Built reusable UI components in React Native and TypeScript, ensuring
+        consistency across the mobile and web applications. I also streamlined
+        data retrieval and caching using GraphQL and Apollo, improving app
+        performance. Also created bug fixes and manage database migrations in
+        Django. Furthermore, I improved monitoring and alerting systems with
+        Terraform and Datadog. My work on AWS S3 buckets led to a significant
+        storage cost reduction by deleting unused data (nearly 9 TB!).
+      </p>
+    ),
     image: `${basePath}/Images/WaveLogo.webp`,
   },
   {
     company: "CBG",
     position: "Programmer Co-op",
     date: "June 2023 - August 2023",
-    description: (<p>I modernized their PHP data acces layer (5.3 to 8.1) for enhanced security and PostgreSQL compatibility. I created integration tests (PHPUnit) and documented testing procedures for future engineers. I also automated testing with bash scripts, streamlining the process and ensuring data consistency. This project improved data security and established a robust testing framework.</p>),
+    description: (
+      <p>
+        I modernized their PHP data acces layer (5.3 to 8.1) for enhanced
+        security and PostgreSQL compatibility. I created integration tests
+        (PHPUnit) and documented testing procedures for future engineers. I also
+        automated testing with bash scripts, streamlining the process and
+        ensuring data consistency. This project improved data security and
+        established a robust testing framework.
+      </p>
+    ),
     image: `${basePath}/Images/CBGLogo.png`,
   },
 ];
@@ -147,20 +168,11 @@ export const skillsData: SkillsData = {
 
 export const projectData = [
   {
-    title: "Project 1",
-    description: "Description 1",
-    image: `${basePath}/Images/CienaLogo.png`,
+    title: "Edge Detection Website",
+    description: (<p>This web application showcases edge detection using JavaScript. Users can upload an image which is then processed directly in the browser to identify and highlight edges through grayscale conversion, Gaussian blurring, and pixel comparison, resulting in a high-contrast image emphasizing outlines.</p>),
+    image: `${basePath}/Images/external-link.png`,
   },
-  {
-    title: "Project 2",
-    description: "Description 2",
-    image: `${basePath}/Images/CienaLogo.png`,
-  },
-  {
-    title: "Project 3",
-    description: "Description 3",
-    image: `${basePath}/Images/CienaLogo.png`,
-  },
+
 ];
 
 // TODO: Add AWS, Oauth2 or auth, maybe okta,
